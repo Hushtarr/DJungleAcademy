336f0cd3fe6f44c90adca185b065705d319b8b2e
hushtar <HUXTAR.EZ@gmail.com>
2024-08-20 22:30:13 +0200

    D'Jungle Academy created

---
+++ src/main/java/com/djungleacademy/DJungleAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.djungleacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DJungleAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DJungleAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/djungleacademy/DJungleAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djungleacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DJungleAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
